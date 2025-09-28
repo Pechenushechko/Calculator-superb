6bb22ffad99ceecc7eb7165f5c04b870d1ae9c00
XaXa <mrhald70@gmail.com>
2025-09-28 15:16:35 +0500

    Сложение

---
+++ Lab1.py
@@ -0,0 +1,9 @@
+while True:
+    a = float(input("Enter a number: "))
+    b = float(input("Enter another number: "))
+    print("Какую операцию вы хотите выполнить?")
+    print("1.Сложение\n2.Вычитание\n3.Умножение\n4.Деление\n5.Выход")
+    c = input("Введите номер операции (1/2/3/4/5): ")
+
+    if c == "1":
+        print(f"Сумма: {a + b}")
